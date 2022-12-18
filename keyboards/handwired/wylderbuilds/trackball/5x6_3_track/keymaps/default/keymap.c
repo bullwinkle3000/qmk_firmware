092081eda9e49986d209ba459bc856ad5328d084
bullwinkle3000 <91431084+bullwinkle3000@users.noreply.github.com>
2022-12-18 18:05:55 -0500

    Prepping 5_3_track build

--- keyboards/handwired/wylderbuilds/trackball/5x6_3_track/keymaps/default/keymap.c
+++ keyboards/handwired/wylderbuilds/trackball/5x6_3_track/keymaps/default/keymap.c
@@ -7,16 +7,20 @@
 #define RAISE MO(_RAISE)
 #define LOWER MO(_LOWER)
 
+/***************************************************************
+ * LEFT SIDE WITH THREE-KEY CLUSTER
+ * RIGHT WITH FOUR-KEY TRACKBALL CLUSTER
+*/
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_5x6(
         KC_ESC , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_BSPC,
         KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                         KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_MINS,
         KC_LSFT, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                         KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
         KC_LCTL, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_BSLASH,
-        KC_GRV, KC_TILDE,KC_LBRC,KC_RBRC,                                                       KC_PLUS, KC_EQL, KC_MINS, KC_RCTL,
-                                         RAISE,KC_SPC,                           KC_ENT, LOWER,
-                                         KC_TAB,KC_HOME,                         KC_END,  KC_DEL,
-                                         KC_BSPC, KC_GRV,                        KC_LGUI, KC_LALT
+                         KC_LBRC,KC_RBRC,                                                       KC_PLUS, KC_EQL,
+                                         RAISE,KC_SPC,KC_BSPC,                  LOWER,
+                                                                                KC_ENT,
+                                                                       KC_LGUI, KC_LALT
     ),
 
     [_LOWER] = LAYOUT_5x6(
@@ -24,10 +28,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,_______,_______,_______,_______,KC_LBRC,                        KC_RBRC, KC_P7 , KC_P8 , KC_P9 ,_______,KC_PLUS,
         _______,KC_HOME,KC_PGUP,KC_PGDN,KC_END ,KC_LPRN,                        KC_RPRN, KC_P4 , KC_P5 , KC_P6 ,KC_MINS,KC_PIPE,
         _______,_______,_______,_______,_______,_______,                        _______, KC_P1 , KC_P2 , KC_P3 ,KC_EQL ,KC_UNDS,
-        _______,_______,_______,KC_PSCR,                                                        _______, KC_P0, _______, _______,
-                                                _______,_______,            _______,_______,
-                                                _______,_______,            _______,_______,
-                                                _______,_______,            _______,QK_BOOT
+                        _______,KC_PSCR,                                                         _______, KC_P0,
+                                        _______,_______,_______,                _______,
+                                                                                _______,
+                                                                        QK_BOOT,_______
 
     ),
 
@@ -36,9 +40,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
           _______,_______,_______,_______,_______,KC_LBRC,                        KC_RBRC,_______,KC_NLCK,KC_INS ,KC_SLCK,KC_MUTE,
           _______,KC_LEFT,KC_UP  ,KC_DOWN,KC_RGHT,KC_LPRN,                        KC_RPRN,KC_MPRV,KC_MPLY,KC_MNXT,_______,KC_VOLU,
           _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,KC_VOLD,
-          _______,_______,_______,_______,                                                        KC_EQL ,_______,_______,_______,
-                                                  _______,_______,            _______,_______,
-                                                  _______,_______,            _______,_______,
-                                                  QK_BOOT,_______,            _______,_______
+                          _______,_______,                                                        KC_EQL ,_______,
+                                          _______,_______,QK_BOOT,                _______,
+                                                                                  _______,
+                                                                          _______,_______
     )
 };
+
+void keyboard_post_init_user(void) {
+    // Customise these values to desired behaviour
+    debug_enable=true;
+    debug_matrix=true;
+    debug_keyboard=true;
+    debug_mouse=true;
+}
